b4e4fcfe78476e682cd0bd6fedafb23949eb1f0b
yevmoga <ext-yevhen.moha@here.com>
2020-08-31 11:39:13 +0300

    bubble

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"algorithms/hash"
+	"algorithms/sort"
 	"fmt"
 )
 
 func main() {
-	h := hash.New(10)
+	arr := []int{100, 3, -19, 99, 1, 17, 99}
 
-	h.Insert(100, 1)
-	h.Insert(101, 2)
-	h.Insert(102, 3)
+	sort.Bubble(arr)
 
-	h.Insert(110, 4)
-	h.Insert(111, 5)
-	h.Insert(112, 6)
-
-	h.Insert(121, 7)
-
-	fmt.Println(h.Search(100))
-	fmt.Println(h.Search(101))
-	fmt.Println(h.Search(110))
-	fmt.Println(h.Search(999))
-
-	h.Delete(100)
-	h.Delete(110)
-	h.Delete(111)
-	h.Delete(999)
+	fmt.Println(arr)
 }
